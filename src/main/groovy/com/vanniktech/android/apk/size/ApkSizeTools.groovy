8a35df01940e0a7c838f6b65217ade926aa1b9a0
Niklas Baudy <niklas.baudy@vanniktech.de>
2016-01-15 14:03:08 +0100

    Fix formatting issue when separator for default local is not a dot

--- src/main/groovy/com/vanniktech/android/apk/size/ApkSizeTools.groovy
+++ src/main/groovy/com/vanniktech/android/apk/size/ApkSizeTools.groovy
@@ -1,14 +1,19 @@
 package com.vanniktech.android.apk.size
 
 import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
 
 final class ApkSizeTools {
     public static String convertBytesToMegaBytes(final long bytes) {
-        return new DecimalFormat("#.##").format(bytes / 1000000);
+        return getDecimalFormat().format(bytes / 1000000)
     }
 
     public static String convertBytesToKiloBytes(final long bytes) {
-        return new DecimalFormat("#.##").format(bytes / 1000);
+        return getDecimalFormat().format(bytes / 1000)
+    }
+
+    private static DecimalFormat getDecimalFormat() {
+        return new DecimalFormat("#.##", new DecimalFormatSymbols(Locale.US))
     }
 
     private ApkSizeTools() {
