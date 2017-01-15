3378d0e1b09315f9acbb698fd60e0407a39fcff5
Jared Burrows <jaredsburrows@gmail.com>
2017-01-14 17:49:11 -0800

    clean up unit tests

--- src/test/groovy/com/vanniktech/android/apk/size/ApkSizeTaskSpec.groovy
+++ src/test/groovy/com/vanniktech/android/apk/size/ApkSizeTaskSpec.groovy
@@ -3,20 +3,21 @@ package com.vanniktech.android.apk.size
 import org.gradle.testfixtures.ProjectBuilder
 import spock.lang.Specification
 
-class ApkSizeTaskSpec extends Specification {
-    def 'canAddTaskToProject'() {
-        given:
-        def project = ProjectBuilder.builder().build()
+final class ApkSizeTaskSpec extends Specification {
+    def project
 
-        expect:
-        def task = project.task('apkSize', type: ApkSizeTask)
-        assert task instanceof ApkSizeTask
+    def 'setup'() {
+        project = ProjectBuilder.builder().build()
     }
 
-    def 'writesOutputFile'() {
+    def 'can add task to project'() {
+        expect:
+        project.task('apkSize', type: ApkSizeTask) instanceof ApkSizeTask
+    }
+
+    def 'writes output file'() {
         given:
-        def project = ProjectBuilder.builder().build()
-        def task = project.task('apkSize', type: ApkSizeTask)
+        ApkSizeTask task = project.task 'apkSize', type: ApkSizeTask
         def parent = new File('./src/test/assets')
         task.apk = new File(parent, 'test.xml')
         task.outputFile = new File(parent, 'test-result.csv')
@@ -26,14 +27,13 @@ class ApkSizeTaskSpec extends Specification {
 
         def scanner = new Scanner(task.outputFile as File)
         def contents = new ArrayList<>()
-
         while (scanner.hasNextLine()) {
             contents.add(scanner.nextLine())
         }
 
         then:
-        assert contents.size() == 2
-        assert contents.get(0) == 'bytes,kilobytes,megabytes'
-        assert contents.get(1) == '219,0.22,0'
+        contents.size() == 2
+        contents.get(0) == 'bytes,kilobytes,megabytes'
+        contents.get(1) == '219,0.22,0'
     }
 }
