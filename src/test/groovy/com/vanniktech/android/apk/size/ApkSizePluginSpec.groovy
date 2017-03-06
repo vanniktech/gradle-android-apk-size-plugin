d4f1db324ee99e394320a6f71c977ab4311c1171
Jared Burrows <jaredsburrows@gmail.com>
2017-03-05 22:11:56 -0800

    add extension to support max apk size

--- src/test/groovy/com/vanniktech/android/apk/size/ApkSizePluginSpec.groovy
+++ src/test/groovy/com/vanniktech/android/apk/size/ApkSizePluginSpec.groovy
@@ -6,32 +6,32 @@ import spock.lang.Specification
 import spock.lang.Unroll
 
 final class ApkSizePluginSpec extends Specification {
-    @Shared def project
+    @Shared project
 
-    def 'setup'() {
+    def "setup"() {
         project = ProjectBuilder.builder().build()
     }
 
-    def 'project is invalid'() {
+    def "project is invalid"() {
         when:
-        new ApkSizePlugin().apply project // apply plugin: 'com.vanniktech.android.apk.size'
+        new ApkSizePlugin().apply(project) // apply plugin: "com.vanniktech.android.apk.size"
 
         then:
         def e = thrown UnsupportedOperationException
-        e.message == 'APK Size Plugin requires the Android Application or Library plugin to be configured'
+        e.message == "APK Size Plugin requires the Android Application or Library plugin to be configured"
     }
 
-    @Unroll def '#projectPlugin project applied correctly'() {
+    @Unroll "#projectPlugin project applied correctly"() {
         given:
         project.apply plugin: projectPlugin
 
         when:
-        project.apply plugin: 'com.vanniktech.android.apk.size'
+        project.apply plugin: "com.vanniktech.android.apk.size"
 
         then:
         noExceptionThrown()
 
         where:
-        projectPlugin << ['com.android.application', 'com.android.library']
+        projectPlugin << ["com.android.application", "com.android.library"]
     }
 }
