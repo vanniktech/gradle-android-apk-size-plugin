53766f9ba7d8be36c0b8301caf179789396ee59f
Jared Burrows <jaredsburrows@gmail.com>
2018-01-25 07:09:30 -0800

    fix spacing in tests

--- src/test/groovy/com/vanniktech/android/apk/size/ApkSizeExtensionSpec.groovy
+++ src/test/groovy/com/vanniktech/android/apk/size/ApkSizeExtensionSpec.groovy
@@ -8,88 +8,88 @@ import org.junit.rules.TemporaryFolder
 import spock.lang.Specification
 
 final class ApkSizeExtensionSpec extends Specification {
-    @Rule final TemporaryFolder tempFolder = new TemporaryFolder()
-    final static COMPILE_SDK_VERSION = 27
-    final static BUILD_TOOLS_VERSION = "27.0.3"
-    final static APPLICATION_ID = "com.example"
-    final static MANIFEST_FILE_TEXT = """<?xml version="1.0" encoding="utf-8"?>
+  @Rule final TemporaryFolder tempFolder = new TemporaryFolder()
+  final static COMPILE_SDK_VERSION = 27
+  final static BUILD_TOOLS_VERSION = "27.0.3"
+  final static APPLICATION_ID = "com.example"
+  final static MANIFEST_FILE_TEXT = """<?xml version="1.0" encoding="utf-8"?>
         <manifest package="com.getkeepsafe.dexcount.integration"
                   xmlns:android="http://schemas.android.com/apk/res/android">
             <application/>
         </manifest>
     """
 
-    Project project
-    File apkFile
+  Project project
+  File apkFile
 
-    def "setup"() {
-        project = ProjectBuilder.builder().build()
-        def manifestFile = new File(project.projectDir, "src/main/AndroidManifest.xml")
-        manifestFile.parentFile.mkdirs()
-        manifestFile.write(MANIFEST_FILE_TEXT)
+  def "setup"() {
+    project = ProjectBuilder.builder().build()
+    def manifestFile = new File(project.projectDir, "src/main/AndroidManifest.xml")
+    manifestFile.parentFile.mkdirs()
+    manifestFile.write(MANIFEST_FILE_TEXT)
 
-        apkFile = tempFolder.newFile("tiles.apk")
-        def apkResource = getClass().getResourceAsStream("/tiles.apk")
-        apkResource.withStream { input ->
-          apkFile.append(input)
-        }
+    apkFile = tempFolder.newFile("tiles.apk")
+    def apkResource = getClass().getResourceAsStream("/tiles.apk")
+    apkResource.withStream { input ->
+      apkFile.append(input)
+    }
+  }
+
+  def "maxApkSize < tiles.apk size, throw exception"() {
+    given:
+    project.apply plugin: "com.android.application"
+    project.apply plugin: "com.vanniktech.android.apk.size"
+    project.android {
+      compileSdkVersion COMPILE_SDK_VERSION
+      buildToolsVersion BUILD_TOOLS_VERSION
+
+      defaultConfig {
+        applicationId APPLICATION_ID
+      }
+    }
+    project.apkSize {
+      maxApkSize = 100 // size of tiles.apk is 1899702
     }
 
-    def "maxApkSize < tiles.apk size, throw exception"() {
-        given:
-        project.apply plugin: "com.android.application"
-        project.apply plugin: "com.vanniktech.android.apk.size"
-        project.android {
-          compileSdkVersion COMPILE_SDK_VERSION
-          buildToolsVersion BUILD_TOOLS_VERSION
+    when:
+    project.evaluate()
 
-          defaultConfig {
-            applicationId APPLICATION_ID
-          }
-        }
-        project.apkSize {
-          maxApkSize = 100 // size of tiles.apk is 1899702
-        }
+    // Override APK file
+    def task = project.tasks.getByName("sizeDebugDebugApk") as ApkSizeTask
+    task.apk = apkFile
+    task.execute()
 
-        when:
-        project.evaluate()
+    then:
+    def e = thrown(GradleException)
+    e.cause instanceof GradleException
+    e.cause.message == "The current APK is 1899702 Kb, the current max is: 100 Kb."
+  }
 
-        // Override APK file
-        def task = project.tasks.getByName("sizeDebugDebugApk") as ApkSizeTask
-        task.apk = apkFile
-        task.execute()
+  def "maxApkSize > tiles.apk size, throw exception"() {
+    given:
+    project.apply plugin: "com.android.application"
+    project.apply plugin: "com.vanniktech.android.apk.size"
+    project.android {
+      compileSdkVersion COMPILE_SDK_VERSION
+      buildToolsVersion BUILD_TOOLS_VERSION
 
-        then:
-        def e = thrown(GradleException)
-        e.cause instanceof GradleException
-        e.cause.message == "The current APK is 1899702 Kb, the current max is: 100 Kb."
+      defaultConfig {
+        applicationId APPLICATION_ID
+      }
+    }
+    project.apkSize {
+      maxApkSize = 2000000 // size of tiles.apk is 1899702
     }
 
-    def "maxApkSize > tiles.apk size, throw exception"() {
-        given:
-        project.apply plugin: "com.android.application"
-        project.apply plugin: "com.vanniktech.android.apk.size"
-        project.android {
-          compileSdkVersion COMPILE_SDK_VERSION
-          buildToolsVersion BUILD_TOOLS_VERSION
+    when:
+    project.evaluate()
 
-          defaultConfig {
-            applicationId APPLICATION_ID
-          }
-        }
-        project.apkSize {
-          maxApkSize = 2000000 // size of tiles.apk is 1899702
-        }
+    // Override APK file
+    def task = project.tasks.getByName("sizeDebugDebugApk") as ApkSizeTask
+    task.apk = apkFile
+    task.execute()
 
-        when:
-        project.evaluate()
-
-        // Override APK file
-        def task = project.tasks.getByName("sizeDebugDebugApk") as ApkSizeTask
-        task.apk = apkFile
-        task.execute()
-
-        then:
-        noExceptionThrown()
-    }
+    then:
+    noExceptionThrown()
+  }
 }
--- src/test/groovy/com/vanniktech/android/apk/size/ApkSizeToolsSpec.groovy
+++ src/test/groovy/com/vanniktech/android/apk/size/ApkSizeToolsSpec.groovy
@@ -4,41 +4,41 @@ import spock.lang.Specification
 import spock.lang.Unroll
 
 final class ApkSizeToolsSpec extends Specification {
-    def defaultLocale = Locale.default
-    def apkSizeTools = new ApkSizeTools()
+  def defaultLocale = Locale.default
+  def apkSizeTools = new ApkSizeTools()
 
-    def "cleanup"() {
-        Locale.setDefault(defaultLocale)
-    }
+  def "cleanup"() {
+    Locale.setDefault(defaultLocale)
+  }
 
-    @Unroll "convert bytes to mega bytes with #locale locale"() {
-        given:
-        Locale.setDefault(locale)
+  @Unroll "convert bytes to mega bytes with #locale locale"() {
+    given:
+    Locale.setDefault(locale)
 
-        expect:
-        apkSizeTools.convertBytesToMegaBytes(10000) == "0.01"
-        apkSizeTools.convertBytesToMegaBytes(655941390323) == "655941.39"
-        apkSizeTools.convertBytesToMegaBytes(594390323) == "594.39"
-        apkSizeTools.convertBytesToMegaBytes(75939284) == "75.94"
-        apkSizeTools.convertBytesToMegaBytes(4843893) == "4.84"
-        apkSizeTools.convertBytesToMegaBytes(843893) == "0.84"
+    expect:
+    apkSizeTools.convertBytesToMegaBytes(10000) == "0.01"
+    apkSizeTools.convertBytesToMegaBytes(655941390323) == "655941.39"
+    apkSizeTools.convertBytesToMegaBytes(594390323) == "594.39"
+    apkSizeTools.convertBytesToMegaBytes(75939284) == "75.94"
+    apkSizeTools.convertBytesToMegaBytes(4843893) == "4.84"
+    apkSizeTools.convertBytesToMegaBytes(843893) == "0.84"
 
-        where:
-        locale << Locale.availableLocales
-    }
+    where:
+    locale << Locale.availableLocales
+  }
 
-    @Unroll "convert bytes to kilo bytes with #locale locale"() {
-        given:
-        Locale.setDefault(locale)
+  @Unroll "convert bytes to kilo bytes with #locale locale"() {
+    given:
+    Locale.setDefault(locale)
 
-        expect:
-        apkSizeTools.convertBytesToKiloBytes(7834843893) == "7834843.89"
-        apkSizeTools.convertBytesToKiloBytes(4843893) == "4843.89"
-        apkSizeTools.convertBytesToKiloBytes(654) == "0.65"
-        apkSizeTools.convertBytesToKiloBytes(22267) == "22.27"
-        apkSizeTools.convertBytesToKiloBytes(5643) == "5.64"
+    expect:
+    apkSizeTools.convertBytesToKiloBytes(7834843893) == "7834843.89"
+    apkSizeTools.convertBytesToKiloBytes(4843893) == "4843.89"
+    apkSizeTools.convertBytesToKiloBytes(654) == "0.65"
+    apkSizeTools.convertBytesToKiloBytes(22267) == "22.27"
+    apkSizeTools.convertBytesToKiloBytes(5643) == "5.64"
 
-        where:
-        locale << Locale.availableLocales
-    }
+    where:
+    locale << Locale.availableLocales
+  }
 }
