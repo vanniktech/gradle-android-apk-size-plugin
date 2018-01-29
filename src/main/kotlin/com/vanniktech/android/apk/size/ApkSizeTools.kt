55174a92efcf7f8c19d3433ba542ccbcc77b4367
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-01-29 15:17:21 +0100

    Add CodeQualityTools.

--- src/main/kotlin/com/vanniktech/android/apk/size/ApkSizeTools.kt
+++ src/main/kotlin/com/vanniktech/android/apk/size/ApkSizeTools.kt
@@ -4,10 +4,13 @@ import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 import java.util.Locale
 
-object ApkSizeTools {
-  fun convertBytesToMegaBytes(bytes: Long): String = getDecimalFormat().format(bytes / 1000000.toDouble())
+private const val BYTES_TO_KILOBYTES = 1000.toDouble()
+private const val BYTES_TO_MEGABYTRES = 1000000.toDouble()
 
-  fun convertBytesToKiloBytes(bytes: Long): String = getDecimalFormat().format(bytes / 1000.toDouble())
+object ApkSizeTools {
+  fun convertBytesToMegaBytes(bytes: Long): String = getDecimalFormat().format(bytes / BYTES_TO_MEGABYTRES)
+
+  fun convertBytesToKiloBytes(bytes: Long): String = getDecimalFormat().format(bytes / BYTES_TO_KILOBYTES)
 
   private fun getDecimalFormat(): DecimalFormat = DecimalFormat("#.##", DecimalFormatSymbols(Locale.US))
 }
