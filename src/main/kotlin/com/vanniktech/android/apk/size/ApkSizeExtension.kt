940e5f468475e40def3ae8b0037efe65beee2028
Daniel Hoeggerl <d.hoeggerl@diamonddogs.cc>
2018-04-25 18:03:05 +0200

    add teamcity support

--- src/main/kotlin/com/vanniktech/android/apk/size/ApkSizeExtension.kt
+++ src/main/kotlin/com/vanniktech/android/apk/size/ApkSizeExtension.kt
@@ -5,4 +5,5 @@ open class ApkSizeExtension {
    * Set the max APK size in Kb. Greater than specified size will fail the build. Default is -1.
    */
   var maxApkSize = -1
+  var teamcity = false
 }
